1d601ee78ba9b4a50539e86dad4bfbc5aa38f097
Solomon Knudson <solomonknudson@gmail.com>
2025-01-05 09:19:18 -0500

    add header guard

--- Engine/ChiliException/include/ChiliException.h
+++ Engine/ChiliException/include/ChiliException.h
@@ -18,7 +18,8 @@
 *	You should have received a copy of the GNU General Public License					  *
 *	along with The Chili DirectX Framework.  If not, see <http://www.gnu.org/licenses/>.  *
 ******************************************************************************************/
-#pragma once
+#ifndef CHILIEXCEPTION_H
+#define CHILIEXCEPTION_H
 #include <string>
 
 class ChiliException
@@ -62,3 +63,4 @@ private:
   std::wstring file;
   unsigned int line;
 };
+#endif // !CHILIEXCEPTION_H
