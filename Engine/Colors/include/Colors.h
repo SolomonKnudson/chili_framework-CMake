bf67192bdcb6556247e139bee5d281eccb81549c
Solomon Knudson <solomonknudson@gmail.com>
2025-01-05 09:22:22 -0500

    add header guard

--- Engine/Colors/include/Colors.h
+++ Engine/Colors/include/Colors.h
@@ -18,8 +18,8 @@
 *	You should have received a copy of the GNU General Public License					  *
 *	along with The Chili DirectX Framework.  If not, see <http://www.gnu.org/licenses/>.  *
 ******************************************************************************************/
-#pragma once
-
+#ifndef COLOR_H
+#define COLOR_H
 class Color
 {
 public:
@@ -30,14 +30,17 @@ public:
     : dword()
   {
   }
+
   constexpr Color(const Color& col)
     : dword(col.dword)
   {
   }
+
   constexpr Color(unsigned int dw)
     : dword(dw)
   {
   }
+
   constexpr Color(unsigned char x,
                   unsigned char r,
                   unsigned char g,
@@ -45,65 +48,79 @@ public:
     : dword((x << 24u) | (r << 16u) | (g << 8u) | b)
   {
   }
+
   constexpr Color(unsigned char r, unsigned char g, unsigned char b)
     : dword((r << 16u) | (g << 8u) | b)
   {
   }
+
   constexpr Color(Color col, unsigned char x)
     : Color((x << 24u) | col.dword)
   {
   }
+
   Color&
   operator=(Color color)
   {
     dword = color.dword;
     return *this;
   }
+
   constexpr unsigned char
   GetX() const
   {
     return dword >> 24u;
   }
+
   constexpr unsigned char
   GetA() const
   {
     return GetX();
   }
+
   constexpr unsigned char
   GetR() const
   {
     return (dword >> 16u) & 0xFFu;
   }
+
   constexpr unsigned char
   GetG() const
   {
     return (dword >> 8u) & 0xFFu;
   }
+
   constexpr unsigned char
   GetB() const
   {
     return dword & 0xFFu;
   }
+
   void
   SetX(unsigned char x)
   {
     dword = (dword & 0xFFFFFFu) | (x << 24u);
   }
+
   void
   SetA(unsigned char a)
   {
     SetX(a);
   }
+
   void
   SetR(unsigned char r)
   {
     dword = (dword & 0xFF00FFFFu) | (r << 16u);
   }
+
   void
+
   SetG(unsigned char g)
   {
     dword = (dword & 0xFFFF00FFu) | (g << 8u);
   }
+
   void
   SetB(unsigned char b)
   {
@@ -118,14 +135,20 @@ namespace Colors
   {
     return (r << 16) | (g << 8) | b;
   }
+
   static constexpr Color White = MakeRGB(255u, 255u, 255u);
   static constexpr Color Black = MakeRGB(0u, 0u, 0u);
+
   static constexpr Color Gray = MakeRGB(0x80u, 0x80u, 0x80u);
   static constexpr Color LightGray = MakeRGB(0xD3u, 0xD3u, 0xD3u);
+
   static constexpr Color Red = MakeRGB(255u, 0u, 0u);
   static constexpr Color Green = MakeRGB(0u, 255u, 0u);
+
   static constexpr Color Blue = MakeRGB(0u, 0u, 255u);
   static constexpr Color Yellow = MakeRGB(255u, 255u, 0u);
+
   static constexpr Color Cyan = MakeRGB(0u, 255u, 255u);
   static constexpr Color Magenta = MakeRGB(255u, 0u, 255u);
 } // namespace Colors
+#endif // !COLOR_H
