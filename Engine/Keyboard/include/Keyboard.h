7107eb6c24eaf929c373f2bda3fe722edb6b6599
Solomon Knudson <solomonknudson@gmail.com>
2025-01-05 09:36:17 -0500

    add header guard

--- Engine/Keyboard/include/Keyboard.h
+++ Engine/Keyboard/include/Keyboard.h
@@ -18,7 +18,9 @@
  *	You should have received a copy of the GNU General Public License					  *
  *	along with The Chili DirectX Framework.  If not, see <http://www.gnu.org/licenses/>.  *
  ******************************************************************************************/
-#pragma once
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+
 #include <bitset>
 #include <queue>
 
@@ -38,35 +40,40 @@ public:
     };
 
   private:
-    Type type;
-    unsigned char code;
+    Type type{};
+    unsigned char code{};
 
   public:
     Event()
-      : type(Type::Invalid)
-      , code(0u)
+      : type{Type::Invalid}
+      , code{0u}
     {
     }
+
     Event(Type type, unsigned char code)
-      : type(type)
-      , code(code)
+      : type{type}
+      , code{code}
     {
     }
+
     bool
     IsPress() const
     {
       return type == Type::Press;
     }
+
     bool
     IsRelease() const
     {
       return type == Type::Release;
     }
+
     bool
     IsValid() const
     {
       return type != Type::Invalid;
     }
+
     unsigned char
     GetCode() const
     {
@@ -79,47 +86,64 @@ public:
   Keyboard(const Keyboard&) = delete;
   Keyboard&
   operator=(const Keyboard&) = delete;
+
   bool
   KeyIsPressed(unsigned char keycode) const;
+
   Event
   ReadKey();
+
   bool
   KeyIsEmpty() const;
+
   char
   ReadChar();
+
   bool
   CharIsEmpty() const;
+
   void
   FlushKey();
   void
   FlushChar();
+
   void
   Flush();
+
   void
   EnableAutorepeat();
+
   void
   DisableAutorepeat();
+
   bool
   AutorepeatIsEnabled() const;
 
 private:
   void
   OnKeyPressed(unsigned char keycode);
+
   void
   OnKeyReleased(unsigned char keycode);
+
   void
   OnChar(char character);
+
   void
   ClearState();
   template <typename T>
+
   void
   TrimBuffer(std::queue<T>& buffer);
 
 private:
-  static constexpr unsigned int nKeys = 256u;
-  static constexpr unsigned int bufferSize = 4u;
-  bool autorepeatEnabled = false;
-  std::bitset<nKeys> keystates;
-  std::queue<Event> keybuffer;
-  std::queue<char> charbuffer;
+  static constexpr unsigned int nKeys{256u};
+  static constexpr unsigned int bufferSize{4u};
+
+  bool autorepeatEnabled{false};
+  std::bitset<nKeys> keystates{};
+
+  std::queue<Event> keybuffer{};
+  std::queue<char> charbuffer{};
 };
+#endif // !KEYBOARD_H
